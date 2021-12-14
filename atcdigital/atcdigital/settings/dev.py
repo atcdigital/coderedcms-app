9e16b65846a931bcc0c635d5b6f88b96fe13d060
Muhammad Abdullah Abid <hi@mabdullahabid.com>
2021-12-14 19:55:43 +0500

    first commit

---
+++ atcdigital/atcdigital/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(#*%q%t*6wc8huag2^5nh-9temg8bkg1_zuauw3s-eop@v8_p3'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
